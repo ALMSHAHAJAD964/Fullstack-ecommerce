b3cd9f8da7933fa111d56e58d2f0f7bbf9833333
shahajad <shahajad.alam@sunday.tech>
2025-06-13 18:24:40 +0530

    devlop deshborad

--- admin/src/components/Sidebar/index.js
+++ admin/src/components/Sidebar/index.js
@@ -1,6 +1,10 @@
 import Button from '@mui/material/Button';
-import { RxDashboard } from "react-icons/rx";
+import { MdDashboard } from "react-icons/md";
 import { FaAngleRight } from "react-icons/fa";
+import { MdProductionQuantityLimits } from "react-icons/md";
+import { TiShoppingCart } from "react-icons/ti";
+import { MdMessage } from "react-icons/md";
+import { IoNotifications } from "react-icons/io5";
 const Sidebar = () => {
     return (
 
@@ -9,8 +13,36 @@ const Sidebar = () => {
                 <ul>
                     <li>
                         <Button className='w-100'>
-                            <span className='icon'><RxDashboard /></span>
+                            <span className='icon'><MdDashboard /></span>
                             Dashboard
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                    </li>
+                     <li>
+                        <Button className='w-100'>
+                            <span className='icon'><MdProductionQuantityLimits /></span>
+                            Products
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                    </li>
+                     <li>
+                        <Button className='w-100'>
+                            <span className='icon'><TiShoppingCart /></span>
+                            Orders
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                    </li>
+                     <li>
+                        <Button className='w-100'>
+                            <span className='icon'><MdMessage /></span>
+                            Messages
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                    </li>
+                     <li>
+                        <Button className='w-100'>
+                            <span className='icon'><IoNotifications /></span>
+                            Notiications
                             <span className='arrow'><FaAngleRight /></span>
                         </Button>
                     </li>
