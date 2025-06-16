6ad7acf49b0195a5a18435af260547e23210ec57
shahajad <shahajad.alam@sunday.tech>
2025-06-16 18:54:30 +0530

    dasborad adding card count

--- admin/src/pages/Dashboard/index.js
+++ admin/src/pages/Dashboard/index.js
@@ -1,7 +1,28 @@
+import { FaUserCircle } from "react-icons/fa";
+import DashboardBox from "./components/dashboardBox";
+
 const Dashboard =() =>{
     return(
         <>
-       
+         <div className="right-content w-100">
+         <div className="row dashboardBoxWrapperRow">
+            <div className="col-md-8">
+                 <div className="dashboardBoxWrapper d-flex">
+             <DashboardBox color={["#000","#48d4"]} icon={<FaUserCircle/>}/>
+             <DashboardBox color={["#000","#ccc"]}/>
+             <DashboardBox color={["#000","#ccc"]}/>
+             <DashboardBox color={["#000","#ccc"]}/>
+         </div>
+            </div>
+
+
+
+            <div className="col-md-4 pl-0">
+               <div className="box">
+               </div>
+            </div>
+         </div>
+         </div>
         </>
     )
 }
