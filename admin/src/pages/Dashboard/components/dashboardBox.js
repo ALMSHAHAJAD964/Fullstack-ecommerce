db9bf94b48763bfb55424e855e312fc672392efa
shahajad <shahajad.alam@sunday.tech>
2025-06-17 16:40:39 +0530

    adding pi chart

--- admin/src/pages/Dashboard/components/dashboardBox.js
+++ admin/src/pages/Dashboard/components/dashboardBox.js
@@ -1,28 +1,93 @@
+import React, { useState } from "react";
+import Button from '@mui/material/Button';
+import { HiDotsVertical } from "react-icons/hi";
+import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import TrendingDownIcon from '@mui/icons-material/TrendingDown';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { IoMdTimer } from "react-icons/io";
 
-import { useEffect } from "react";
+const options = ['Last Day', 'Last week ', 'Last month','Last Year'];
+const ITEM_HEIGHT = 48;
+
 const DashboardBox = (props) => {
-    return (
-        <>
-            <div className="dashboardBox" style={{
-               backgroundImage:`linear-gradient(to right,${props.color?.[0]},${props.color?.[1]})`}}>
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
 
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-                <div className="d-flex w-100">
-                    <div className="col1">
-                       <h4 className="text-white mb-0">Total Users</h4>
-                       <span className="text-white">277</span>
-                    </div>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
-                    <div className="ml-auto">
-                        <span className="icon">
-                        {props.icon}
-                    </span>
-                        
+  return (
+    <>
+      <Button
+        className="dashboardBox"
+        style={{
+          backgroundImage: `linear-gradient(to right,${props.color?.[0]},${props.color?.[1]})`,
+        }}
+      >
+        {props.grow === true ? (
+          <span className="chart"><TrendingUpIcon /></span>
+        ) : (
+          <span className="chart"><TrendingDownIcon /></span>
+        )}
 
-                    </div>
-                </div>
-               </div>
-        </>
-    );
-}
+        <div className="d-flex w-100">
+          <div className="col1">
+            <h4 className="text-white mb-0">Total Users</h4>
+            <span className="text-white">277</span>
+          </div>
+
+          <div className="ml-auto">
+            {props.icon && <span className="icon">{props.icon}</span>}
+          </div>
+        </div>
+
+        <div className="d-flex alignitems-center w-100 bottomEle">
+          <h6 className="text-white mb-0 mt-0">Last month</h6>
+          <div className="ml-auto">
+            <Button
+              className="ml-auto toggleIcon"
+              id="long-button"
+              aria-controls={open ? 'long-menu' : undefined}
+              aria-expanded={open ? 'true' : undefined}
+              aria-haspopup="true"
+              onClick={handleClick}
+            >
+              <HiDotsVertical />
+            </Button>
+          </div>
+        </div>
+      </Button>
+
+      <Menu
+       className="Dropdown_menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+      {options.map((option) => (
+  <MenuItem key={option} onClick={handleClose}>
+    <IoMdTimer style={{ marginRight: '8px' }} />
+    {option}
+  </MenuItem>
+))}
+      </Menu>
+    </>
+  );
+};
+
 export default DashboardBox;
--- admin/src/pages/Dashboard/index.js
+++ admin/src/pages/Dashboard/index.js
@@ -1,29 +1,126 @@
-import { FaUserCircle } from "react-icons/fa";
+import React, { useState } from "react";
 import DashboardBox from "./components/dashboardBox";
+import { FaUserCircle } from "react-icons/fa";
+import { IoMdCart, IoMdTimer } from "react-icons/io";
+import { MdShoppingBag } from "react-icons/md";
+import { GiStarsStack } from "react-icons/gi";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import { HiDotsVertical } from "react-icons/hi";
+import { Chart } from "react-google-charts";
 
-const Dashboard =() =>{
-    return(
-        <>
-         <div className="right-content w-100">
-         <div className="row dashboardBoxWrapperRow">
-            <div className="col-md-8">
-                 <div className="dashboardBoxWrapper d-flex">
-             <DashboardBox color={["#000","#48d4"]} icon={<FaUserCircle/>}/>
-             <DashboardBox color={["#000","#ccc"]}/>
-             <DashboardBox color={["#000","#ccc"]}/>
-             <DashboardBox color={["#000","#ccc"]}/>
-         </div>
+const data = [
+    ["Year", "Sales", "Expense"],
+    ["2013", 1000, 400],
+    ["2014", 1170, 460],
+    ["2015", 660, 1120],
+    ["2016", 1030, 540],
+];
+
+const options1 = {
+    backgroundColor: "transparent",
+    'chartArea':{'width':'100%','height':'100%'}
+};
+
+const options = ['Last Day', 'Last week', 'Last month', 'Last Year'];
+const ITEM_HEIGHT = 48;
+
+const Dashboard = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <div className="right-content w-100">
+            <div className="row dashboardBoxWrapperRow">
+                {/* Left Boxes */}
+                <div className="col-md-8">
+                    <div className="dashboardBoxWrapper d-flex">
+                        <DashboardBox color={["#1da256", "#48d483"]} icon={<FaUserCircle />} grow={true} />
+                        <DashboardBox color={["#c0123e", "#ed64fe"]} icon={<IoMdCart />} />
+                        <DashboardBox color={["#2c78e5", "#60aff5"]} icon={<MdShoppingBag />} />
+                        <DashboardBox color={["#e1950e", "#f3cd29"]} icon={<GiStarsStack />} />
+                    </div>
+                </div>
+
+                {/* Right Graph + Dropdown */}
+                <div className="col-md-4 pl-0">
+                    <div className="box graphBox p-3 bg-dark text-white rounded">
+                        <div className="d-flex align-items-center justify-content-between mb-3">
+                            <div>
+                                <h4 className="mb-1">Total Sales</h4>
+                                <h3 className="font-weight-bold">$343,441.00</h3>
+                                <p>$343,441.30 in last month</p>
+                            </div>
+                            <Button
+                                id="long-button"
+                                aria-controls={open ? 'long-menu' : undefined}
+                                aria-expanded={open ? 'true' : undefined}
+                                aria-haspopup="true"
+                                onClick={handleClick}
+                            >
+                                <HiDotsVertical className="text-white" />
+                            </Button>
+                        </div>
+
+                        <Chart
+                            chartType="PieChart"
+                            width="100%"
+                            height="170px"
+                            data={data}
+                            options={options1}
+                        />
+
+                        <Menu
+                            className="Dropdown_menu"
+                            anchorEl={anchorEl}
+                            open={open}
+                            onClose={handleClose}
+                            MenuListProps={{
+                                'aria-labelledby': 'long-button',
+                            }}
+                            PaperProps={{
+                                style: {
+                                    maxHeight: ITEM_HEIGHT * 4.5,
+                                    width: '20ch',
+                                },
+                            }}
+                        >
+                            {options.map((option) => (
+                                <MenuItem key={option} onClick={handleClose}>
+                                    <IoMdTimer style={{ marginRight: '8px' }} />
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </div>
+                </div>
             </div>
 
 
 
-            <div className="col-md-4 pl-0">
-               <div className="box">
-               </div>
+
+            <div className="card shadow border-0 p-3 mt-4">
+                <h3 className="hd">Best Selling Products</h3>
+
+
+<div className="row">
+    <div className="row">
+        <h4>SHOW BY</h4>
+    </div>
+</div>
+
             </div>
-         </div>
-         </div>
-        </>
-    )
-}
+        </div>
+    );
+};
+
 export default Dashboard;
